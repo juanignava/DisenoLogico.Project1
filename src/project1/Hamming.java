1291b9ea4ea76968888f437a9dd9f0569a66f135
LuisPeMoraRod <luismorarod98@gmail.com>
2021-03-20 23:58:13 -0600

    Add last row to Hamming table

--- src/project1/Hamming.java
+++ src/project1/Hamming.java
@@ -14,23 +14,74 @@ public class Hamming {
         hammingPanel.setBorder(BorderFactory.createTitledBorder(
                 BorderFactory.createEtchedBorder(), "Table 3. Calculation of Hamming coded parity bits", TitledBorder.CENTER,
                 TitledBorder.TOP));
+        String[] row0 = this.getWord(binaryNumber);
+        String[] row1 = this.getParityBit1(binaryNumber, pairParity);
+        String[] row2 = this.getParityBit2(binaryNumber, pairParity);
+        String[] row3 = this.getParityBit3(binaryNumber, pairParity);
+        String[] row4 = this.getParityBit4(binaryNumber, pairParity);
+        String[] row5 = this.getParityBit5(binaryNumber, pairParity);
+        String[] word = this.getCodedWord(row1, row2, row3, row4, row5);
 
-        String[] row1 = getParityBit1(binaryNumber, pairParity);
-        String[] row2 = getParityBit2(binaryNumber, pairParity);
-        String[] row3 = getParityBit3(binaryNumber, pairParity);
-        String[] row4 = getParityBit4(binaryNumber, pairParity);
-        String[] row5 = getParityBit5(binaryNumber, pairParity);
-
-        String[][] results = { row1, row2, row3, row4, row5};
+        String[][] results = { row0, row1, row2, row3, row4, row5, word};
 
         String[] headers = { "  ", "P1", "P2", "D1", "P3", "D2", "D3", "D4", "P4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "P5", "D12"};
         JTable table = new JTable(results, headers);
+        table.getColumnModel().getColumn(0).setPreferredWidth(800);
+        table.getColumnModel().getColumn(14).setPreferredWidth(150);
+        table.getColumnModel().getColumn(15).setPreferredWidth(150);
+        table.getColumnModel().getColumn(17).setPreferredWidth(150);
         hammingPanel.add(new JScrollPane(table), Component.CENTER_ALIGNMENT);
     }
 
     public Hamming(String word){
         String[] bits = getParityBit5(word, true);
         for (int i = 0; i < bits.length; i++) System.out.print(bits[i]);
+    }
+
+    private String[] getWord(String binaryNumber){
+        String[] word = new String[18];
+        word[0] = "Word";
+        word[1] = " ";
+        word[2] = " ";
+        word[3] = String.valueOf(binaryNumber.charAt(0));
+        word[4] = " ";
+        word[5] = String.valueOf(binaryNumber.charAt(1));
+        word[6] = String.valueOf(binaryNumber.charAt(2));
+        word[7] = String.valueOf(binaryNumber.charAt(3));
+        word[8] = " ";
+        word[9] = String.valueOf(binaryNumber.charAt(4));
+        word[10] = String.valueOf(binaryNumber.charAt(5));
+        word[11] = String.valueOf(binaryNumber.charAt(6));
+        word[12] = String.valueOf(binaryNumber.charAt(7));
+        word[13] = String.valueOf(binaryNumber.charAt(8));
+        word[14] = String.valueOf(binaryNumber.charAt(9));
+        word[15] = String.valueOf(binaryNumber.charAt(10));
+        word[16] = " ";
+        word[17] = String.valueOf(binaryNumber.charAt(11));
+        return word;
+    }
+
+    private String[] getCodedWord(String[] row1, String[] row2, String[] row3, String[] row4, String[] row5){
+        String[] word = new String[18];
+        word[0] = "Coded Word";
+        word[1] = row1[1];
+        word[2] = row2[2];
+        word[3] = row2[3];
+        word[4] = row3[4];
+        word[5] = row3[5];
+        word[6] = row3[6];
+        word[7] = row3[7];
+        word[8] = row4[8];
+        word[9] = row4[9];
+        word[10] = row4[10];
+        word[11] = row4[11];
+        word[12] = row4[12];
+        word[13] = row4[13];
+        word[14] = row4[14];
+        word[15] = row4[15];
+        word[16] = row5[16];
+        word[17] = row5[17];
+        return word;
     }
 
     private String[] getParityBit1(String binaryNumber, boolean pairParity){
