1b8c3a9ba6224a552dee4cb78c8cc318106dffa4
NachoGranados <ignaciogramar@gmail.com>
2021-03-21 14:42:37 -0600

    Add Hamming decoding

--- src/project1/Hamming.java
+++ src/project1/Hamming.java
@@ -33,6 +33,65 @@ public class Hamming {
         table.getColumnModel().getColumn(15).setPreferredWidth(150);
         table.getColumnModel().getColumn(17).setPreferredWidth(150);
         hammingPanel.add(new JScrollPane(table), Component.CENTER_ALIGNMENT);
+
+        String newBinaryNumber = modifiedBinaryNumber(binaryNumber, Integer.parseInt(errorBitField));
+
+        JPanel decodingHammingPanel = new JPanel();
+        panel.add(decodingHammingPanel);
+
+        decodingHammingPanel.setBorder(BorderFactory.createTitledBorder(
+                                       BorderFactory.createEtchedBorder(), "Table 3. Calculation of Hamming decoded parity bits",
+                                       TitledBorder.CENTER, TitledBorder.TOP));
+
+        //rows for table 3 (Hamming decodification)
+        String[] decodedRow0 = this.getWord(newBinaryNumber);
+        String[] decodedRow1 = this.getParityBit1(newBinaryNumber, pairParity);
+        String[] decodedRow2 = this.getParityBit2(newBinaryNumber, pairParity);
+        String[] decodedRow3 = this.getParityBit3(newBinaryNumber, pairParity);
+        String[] decodedRow4 = this.getParityBit4(newBinaryNumber, pairParity);
+        String[] decodedRow5 = this.getParityBit5(newBinaryNumber, pairParity);
+
+        String[] codedParityBits = {row1[1], row2[2], row3[4], row4[8], row5[16]};
+        String[] decodedParityBits = {decodedRow1[1], decodedRow2[2], decodedRow3[4], decodedRow4[8], decodedRow5[16]};
+
+        String[] finalDecodedRow0 = this.addDecodedColumns(decodedRow0, " ", " ");
+        String[] finalDecodedRow1 = this.addDecodedColumns(decodedRow1, decodedParityBits[0], codedParityBits[0]);
+        String[] finalDecodedRow2 = this.addDecodedColumns(decodedRow2, decodedParityBits[1], codedParityBits[1]);
+        String[] finalDecodedRow3 = this.addDecodedColumns(decodedRow3, decodedParityBits[2], codedParityBits[2]);
+        String[] finalDecodedRow4 = this.addDecodedColumns(decodedRow4, decodedParityBits[3], codedParityBits[3]);
+        String[] finalDecodedRow5 = this.addDecodedColumns(decodedRow5, decodedParityBits[4], codedParityBits[4]);
+
+        String[] errorResults = { Character.toString(finalDecodedRow5[20].charAt(0)),
+                                  Character.toString(finalDecodedRow4[20].charAt(0)),
+                                  Character.toString(finalDecodedRow3[20].charAt(0)),
+                                  Character.toString(finalDecodedRow2[20].charAt(0)),
+                                  Character.toString(finalDecodedRow1[20].charAt(0))};
+
+        String[][] decodingResults = { finalDecodedRow0, finalDecodedRow1, finalDecodedRow2, finalDecodedRow3, finalDecodedRow4,
+                                       finalDecodedRow5};
+
+        String[] decodingHeaders = { "  ", "P1", "P2", "D1", "P3", "D2", "D3", "D4", "P4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "P5", "D12",
+                                     "Decoded Parity", "Coded Parity", "Error Calculation"};
+
+        JTable decodingTable = new JTable(decodingResults, decodingHeaders);
+        decodingTable.getColumnModel().getColumn(0).setPreferredWidth(300);
+        decodingTable.getColumnModel().getColumn(14).setPreferredWidth(150);
+        decodingTable.getColumnModel().getColumn(15).setPreferredWidth(150);
+        decodingTable.getColumnModel().getColumn(17).setPreferredWidth(150);
+        decodingTable.getColumnModel().getColumn(18).setPreferredWidth(150);
+        decodingTable.getColumnModel().getColumn(19).setPreferredWidth(150);
+        decodingTable.getColumnModel().getColumn(20).setPreferredWidth(150);
+        decodingHammingPanel.add(new JScrollPane(decodingTable), Component.CENTER_ALIGNMENT);
+
+        String errorResultStr = this.createNumber(errorResults);
+
+        String errorText = "Parity error -> P5 P4 P3 P2 P1 -> " + errorResults[0] + " " + errorResults[1] + " " + errorResults[2] + " " + errorResults[3] +
+                           " " + errorResults[4] + " = " + this.getDecimalValue(errorResultStr) + " -> bit error detected in position " + this.getDecimalValue(errorResultStr);
+
+        JLabel errorLabel = new JLabel(errorText, SwingConstants.CENTER);
+        errorLabel.setFont(new Font("Calibri", Font.PLAIN, 18));
+        panel.add(errorLabel);
+
     }
 
     /**
@@ -298,7 +357,7 @@ public class Hamming {
     /**
      * Counts the number of '1' char in a string
      * @param word String
-     * @return     number of '1' char that word string contains
+     * @return number of '1' char that word string contains
      */
     private int countOnes(String word){
         int count = 0;
@@ -308,6 +367,123 @@ public class Hamming {
             }
         }
         return count;
+    }
+
+    /**
+     * Changes the specified bit position to generate an error in the binaryNumber
+     * @param oldBinaryNumber  String with the 12 bit binary digit
+     * @param errorBitPosition
+     * @return String the binary number with the eror bit.
+     */
+    private String modifiedBinaryNumber(String oldBinaryNumber, int errorBitPosition) {
+
+        String newBinaryNumber = "";
+
+        for (int i = 0; i < 12; i++) {
+
+            if (i == errorBitPosition) {
+
+                if (String.valueOf(oldBinaryNumber.charAt(i)).equals("0")) {
+
+                    newBinaryNumber += "1";
+
+                } else {
+
+                    newBinaryNumber += "0";
+
+                }
+
+            } else {
+
+                newBinaryNumber += String.valueOf(oldBinaryNumber.charAt(i));
+
+            }
+
+        }
+
+        return newBinaryNumber;
+
+    }
+
+    /**
+     * Adds the three missing error validation columns
+     * @param oldRow
+     * @param decodedBit
+     * @param codedBit
+     * @return resized received row
+     */
+    private String[] addDecodedColumns(String[] oldRow, String decodedBit, String codedBit) {
+
+        String[] newRow = new String[21];
+
+        for (int i = 0; i < 18; i++) {
+
+            newRow[i] = oldRow[i];
+
+        }
+
+        newRow[18] = decodedBit;
+        newRow[19] = codedBit;
+
+        if (decodedBit.equals(" ") & codedBit.equals(" ")) {
+
+            newRow[20] = " ";
+
+        } else  if (decodedBit.equals(codedBit)) {
+
+            newRow[20] = "0 OK";
+
+        } else {
+
+            newRow[20] = "1 Error";
+
+        }
+
+        return newRow;
+
+    }
+
+    /**
+     * Convert all the array elements in one string
+     * @param errorBits
+     * @return string with all array elements
+     */
+    private String createNumber(String[] errorBits) {
+
+        String number = "";
+
+        for(int i = 0; i < errorBits.length; i++) {
+
+            number += errorBits[i];
+
+        }
+
+        return number;
+
+    }
+
+    /**
+     * Convert the received binaty number in a decimal number
+     * @param binaryNumber
+     * @return integer decimal number
+     */
+    private int getDecimalValue(String binaryNumber) {
+
+        int bnLength = binaryNumber.length();
+        int j = bnLength - 1;
+        int decimal = 0;
+        int bit;
+        char singleBit;
+
+        for(int i = 0; i < bnLength; i++){
+            singleBit = binaryNumber.charAt(i);
+            bit = Character.getNumericValue(singleBit);
+            decimal = (int) (decimal + Math.pow(2, j)*bit);
+            j = j - 1;
+        }
+
+        return decimal;
+
     }
 
 }
